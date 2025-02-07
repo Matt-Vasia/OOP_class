0b2ac39f7fc0ed841c30918b055bae905bc303b5
Matt-Vasia <matas_vasiliauskas@outlook.com>
2025-02-07 12:29:22 +0200

    v.pradine
    push

---
+++ template.h
@@ -0,0 +1,16 @@
+#ifndef TEMPLATE_H_INCLUDED
+#define TEMPLATE_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+#endif // TEMPLATE_H_INCLUDED
+
+
