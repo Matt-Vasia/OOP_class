48912f165ef30e0314c8e53eac05962446c09b9a
Matt-Vasia <matas_vasiliauskas@outlook.com>
2025-02-07 18:47:47 +0200

    reads one student's info

--- template.h
+++ template.h
@@ -6,13 +6,18 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
 using std::vector;
-
+using std::ifstream;
+using std::setprecision;
+using std::fixed;
+using std::stringstream;
+using std::terminate;
 
 struct duom
 {
@@ -23,6 +28,29 @@ struct duom
     double vid=0, med=0;
 };
 
+void read(duom &laik, vector <duom> &grupe)
+{
+        char line[256];
+        cin.getline(line, 256);
+        stringstream s;
+        s << line;
+        s >> laik.var >> laik.pav;
+        if(!s)
+        {
+            cout<<"Duomenų failas neatsidaro"<<endl;
+            terminate();
+        }
+        while(s)
+        {
+            int temp;
+            s>>temp;
+            laik.pazymiai.push_back(temp); //saug 0-10 pazymiai
+        }
+        laik.exam=laik.pazymiai[laik.pazymiai.size()-1];
+        laik.pazymiai.pop_back();
+        grupe.push_back(laik);
+    }
+}
 
 #endif // TEMPLATE_H_INCLUDED
 
--- uzd.cpp
+++ uzd.cpp
@@ -1,8 +1,11 @@
 #include "template.h"
 
+void read(duom &laik, vector <duom> &grupe);
+
 int main(){
     duom laik;
     vector <duom> grupe;
+    read(laik, grupe);
     return 0;
 }
 
