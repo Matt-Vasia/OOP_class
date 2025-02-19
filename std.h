f63070b5819659c39d05b63f8366f02f45340f5d
Matt-Vasia <matas_vasiliauskas@outlook.com>
2025-02-19 13:16:21 +0200

    cpp/h files, sorting fixes
    sutvarkytas rasymas (blogai su vardais ir pavardem rusiavo)
    cpp, h files

---
+++ std.h
@@ -0,0 +1,19 @@
+#ifndef STD_H_INCLUDED
+#define STD_H_INCLUDED
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::vector;
+using std::ifstream;
+using std::setprecision;
+using std::fixed;
+using std::istringstream;
+using std::terminate;
+using std::sort;
+using std::setw;
+using std::left;
+using std::stringstream;
+
+#endif
--- uzd.cpp
+++ uzd.cpp
@@ -1,15 +1,4 @@
-#include "template.h"
-
-void menu_with_read(vector <duom> &grupe);
-double average(duom given);
-double median(duom given);
-void random(vector <duom> &grupe, int m);
-void random_full(vector <duom> &grupe);
-void read_file(vector <duom> &grupe);
-void read_half(vector <duom> &grupe);
-void vid_med_calc(vector <duom> &grupe);
-void sorting(vector <duom> &grupe);
-void print(vector <duom> &grupe);
+#include "template.cpp"
 
 int main(){
     vector <duom> grupe;
