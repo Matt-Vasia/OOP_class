95bbbb572f47e17eccf40b3889c6ee00412bd751
Matt-Vasia <matas_vasiliauskas@outlook.com>
2025-03-16 11:44:27 +0200

    list, deque, vector subfolders

---
+++ vector/uzd.cpp
@@ -0,0 +1,18 @@
+#include "template.cpp"
+
+int main(){
+    vector <duom> grupe;
+    ///
+    menu(grupe);
+    ///
+    vid_med_calc(grupe);
+    ///
+    sorting(grupe, '\0');
+    ///
+    print_answers(grupe);
+    return 0;
+}
+
+
+
+
